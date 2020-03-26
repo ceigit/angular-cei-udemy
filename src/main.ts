52b2c21116bb87d13204c148fdca9b6b59cfd925
ceigit <ceigithub@gmail.com>
2020-03-26 19:26:22 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { MasterModule } from './home/ca-master.module';
+import { MasterModule } from './customer-app/home/ca-master.module';
 
 platformBrowserDynamic().bootstrapModule(MasterModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
